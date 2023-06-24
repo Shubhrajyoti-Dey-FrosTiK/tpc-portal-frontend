8aa0b7c98cf89e217f61169095403b01c56bfa6b
Shubhrajyoti Dey <shubhrajyotidey@Apples-MacBook-Pro.local>
2023-06-24 23:24:50 +0530

    Added domain to jaf

--- app/company/iaf/page.tsx
+++ app/company/iaf/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Form from "../../../components/form/Form";
 import IAFSchema from "../../../configs/IAFSchema";
 import { selectIdStore } from "../../../store/states/idStore";
+import { selectUser } from "../../../store/states/userSlice";
 
 // export const runtime = "edge";
 
 function IAF() {
   const IdStore = useSelector(selectIdStore);
+  const User = useSelector(selectUser);
+  const [domain, setDomain] = useState<string>("");
+
+  const fetchEmail = async () => {
+    const email = User.currentUser.email;
+    if (email && email.split("@").length > 1) {
+      setDomain(`@${email.split("@")[1]}`);
+    }
+  };
+
+  useEffect(() => {
+    if (User.currentUser) {
+      fetchEmail();
+    }
+  }, [User.currentUser]);
 
   return (
     <div className="max-w-[800px] m-auto pl-5 pr-5">
@@ -18,6 +34,7 @@ function IAF() {
         bodyTemplate={{
           recruiter: IdStore.recruiterId,
           company: IdStore.companyId,
+          domain: { domain },
         }}
         postUrl={`${process.env.NEXT_PUBLIC_IAF_JAF_BACKEND}/iaf` || ""}
       />
--- app/company/jaf/page.tsx
+++ app/company/jaf/page.tsx
@@ -1,15 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Form from "../../../components/form/Form";
 import JAFSchema from "../../../configs/JAFSchema";
 import { selectIdStore } from "../../../store/states/idStore";
+import { selectUser } from "../../../store/states/userSlice";
 
 // export const runtime = "edge";
 
 function JAF() {
   const IdStore = useSelector(selectIdStore);
+  const User = useSelector(selectUser);
+  const [domain, setDomain] = useState<string>("");
+
+  const fetchEmail = async () => {
+    const email = User.currentUser.email;
+    if (email && email.split("@").length > 1) {
+      setDomain(`@${email.split("@")[1]}`);
+    }
+  };
+
+  useEffect(() => {
+    if (User.currentUser) {
+      fetchEmail();
+    }
+  }, [User.currentUser]);
 
   return (
     <div className="max-w-[800px] m-auto pl-5 pr-5">
@@ -18,6 +34,7 @@ function JAF() {
         bodyTemplate={{
           recruiter: IdStore.recruiterId,
           company: IdStore.companyId,
+          domain: { domain },
         }}
         postUrl={`${process.env.NEXT_PUBLIC_IAF_JAF_BACKEND}/jaf` || ""}
       />
